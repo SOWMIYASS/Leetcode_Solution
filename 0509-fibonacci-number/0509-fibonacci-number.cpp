6f5ab9abc64534e5a4ff71ebc2dcffcba92e8afa
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-01-31 21:27:36 +0530

    Time: 3 ms (69.29%), Space: 5.9 MB (66.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,7 +3,13 @@ public:
     int fib(int n) {
         if(n==0) return 0;
         if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        int result_array[n+1];
+        result_array[0]=0;
+        result_array[1]=1;
+        for(int i=2;i<=n;i++){
+            result_array[i]=result_array[i-1]+result_array[i-2];
+        }
+       return  result_array[n];
         
     }
 };
