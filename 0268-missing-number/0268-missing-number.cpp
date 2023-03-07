f32b58ec27fd4d538dc8a9c85688ac04e7278468
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-03-07 06:47:35 +0530

    Time: 52 ms (8.38%), Space: 25.8 MB (5.51%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        map<int,int>m;
+        for(auto i:nums)
+            m[i]=1;
+        for(int i=0;i<=nums.size();i++){
+            if(m[i]==0) 
+                return i;
+        }
+        return 0;
+    }
+    
+};
