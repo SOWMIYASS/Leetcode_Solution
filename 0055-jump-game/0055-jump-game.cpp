1290fda615b3b50251740cdf0239410bbdce7a2a
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-06-10 11:10:44 +0530

    Time: 74 ms (54.97%), Space: 48.4 MB (42.00%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int val=0;
+        for(int i=nums.size()-2;i>=0;i--){
+            val++;
+            if(nums[i]>=val) val=0;
+        }
+        if(val==0) return true;
+        else return false;
+        
+    }
+};
