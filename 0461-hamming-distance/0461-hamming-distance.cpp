aeaa02545e73eb28d295439be47ce73f6ae37c6e
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-06-26 12:19:14 +0530

    Time: 3 ms (36.05%), Space: 5.8 MB (100.00%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -3,10 +3,9 @@ public:
     int hammingDistance(int x, int y) {
         int n=x^y;
         int ans=0;
-        long long int c=1;
-        for(int i=0;i<32;i++){
-           if(n&c)ans++;
-            c=c<<1;
+        long long int i;
+        for(i=1;i<=n;i=i<<1){
+           if(n&i)ans++;
         }
         return ans;
     }
