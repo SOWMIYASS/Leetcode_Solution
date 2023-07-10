c886c5bccfd45118f0f205f4fbe5de8a7b471e88
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-07-10 21:18:39 +0530

    Time: 270 ms (55.36%), Space: 85.6 MB (15.30%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -3,10 +3,9 @@ public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>>answer;
         vector<pair<double,vector<int>>>d;
-        for(auto i:points)
-        {
-            double distance=sqrt(i[0]*i[0]+i[1]*i[1]);
-            d.push_back({distance,i});
+        for(auto i:points){
+            double dist=sqrt(i[0]*i[0]+i[1]*i[1]);
+            d.push_back({dist,i});
         }
         sort(d.begin(),d.end());
         for(int i=0;i<k;i++) answer.push_back(d[i].second);
