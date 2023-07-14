a8aeedeeb536334cff1ab50263b059c65ec90261
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-07-14 11:10:40 +0530

    Time: 17 ms (6.22%), Space: 13.8 MB (5.75%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>result;
+       if(nums.size()==0) return {-1,-1};
         vector<int>ans;
+        int count=0,j=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==target)
-                result.push_back(i);
+            if(nums[i]==target) ans.push_back(i);
         }
-        int n=result.size();
-        if(result.empty()){
-            result.push_back(-1);
-            result.push_back(-1);
+        if(ans.size()==0) return {-1,-1};
+        else if(ans.size()==1) {
+            ans.push_back(ans[0]);
+            return ans;
         }
-        if(result.size()==1){
-            result.push_back(result[0]);
-        }
-        if(n>=3){
-            int a=0;
-            for(int i=0;i<=1;i++){
-                if(i==0){
-                ans.push_back(result[0]);
-                }
-                if(i==1){
-                    ans.push_back(result[n-1]);
-                }
-            }
-            return ans; 
-        }
-        return result;
+        else if(ans.size()>=2) return{ans[0],ans[ans.size()-1]};
+        else return ans;
+
     }
 };
