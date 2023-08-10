f656c7656ccb1971a5400f602745bf575ec8595e
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-08-10 19:02:08 +0530

    Time: 3 ms (42.63%), Space: 6.5 MB (37.70%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        int n=s.size();
-        int i=n-1;
+        int i=s.size()-1;
         int count=0;
-        while(i>=0&&s[i]==' ') i--;
-        while(i>=0 && s[i]!=' '){
-            count++;
-            i--;
-        }
+        while(i>=0 &&s[i]==' ') i--;
+        //cout<<i<<" ";
+        while(i>=0&&s[i--]!=' ') count++;
         return count;
     }
 };
