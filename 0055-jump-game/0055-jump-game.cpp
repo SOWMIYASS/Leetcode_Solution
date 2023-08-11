62a6e304b9e94f0fb9696e6e9280e953a973f996
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-08-11 11:37:06 +0530

    Time: 49 ms (88.95%), Space: 48.5 MB (41.41%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,12 +4,9 @@ public:
         int val=0;
         for(int i=nums.size()-2;i>=0;i--){
             val++;
-            if(nums[i]>=val) {
-                val=0;
-            }
+            if(nums[i]>=val) val=0;
         }
         if(val==0) return true;
         else return false;
-        
     }
 };
