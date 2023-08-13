7ab241c92e0b3bdbc471014c82c947a70e6fa589
sowmiya_13 <109498034+SOWMIYASS@users.noreply.github.com>
2023-08-13 21:56:40 +0530

    Time: 4 ms (27.29%), Space: 8.6 MB (6.93%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-    int n = nums.size();
-    vector<int> target(n, -1); // Initialize the target array with -1
-    
-    for (int i = 0; i < n; i++) {
-        if (target[index[i]] == -1) {
-            target[index[i]] = nums[i];
-        } else {
-            // Shift elements to the right to make space for the new element
-            for (int j = n - 1; j > index[i]; j--) {
-                target[j] = target[j - 1];
-            }
-            target[index[i]] = nums[i];
-        }
-    }
-    
-    return target;
+       int n=nums.size();
+       vector<int>res(n,-1);
+       for(int i=0;i<n;i++){
+           if(res[index[i]]==-1)
+               res[index[i]]=nums[i];
+           else{
+               for(int j=n-1;j>index[i];j--)
+                   res[j]=res[j-1];
+               
+               res[index[i]]=nums[i];
+           }
+       }
+       return res;
+       
 }
 
 };
